db0aa4990474a77a1e8ce07029dc30de9ba64472
Gumging <aidenyao1992@icloud.com>
2021-04-02 22:47:29 -0400

    123

--- ArrayADT/ArrayADT.cpp
+++ ArrayADT/ArrayADT.cpp
@@ -165,9 +165,181 @@ float Avg(struct Array arr)
     return (float)Sum(arr) / arr.length;
 }
 
+void Reverse(struct Array* arr)
+{
+    int* B;
+    int i, j;
+
+    B = (int*)malloc(arr->length * sizeof(int));
+    for (i = arr->length - 1, j = 0; i >= 0; i--, j++)
+        B[j] = arr->A[i];
+    for (i = 0; i < arr->length; i++)
+        arr->A[i] = B[i];
+
+}
+
+void Reverse2(struct Array* arr)
+{
+    int i, j;
+    for (i = 0, j = arr->length - 1; i < j; i++, j--)
+    {
+        swap(&arr->A[i], &arr->A[j]);
+    }
+}
+
+void InsertSort(struct Array* arr, int x)
+{
+    int i = arr->length - 1;
+    if (arr->length == arr->size)
+        return;
+    while (i>=0 && arr->A[i] > x)
+    {
+        arr->A[i + 1] = arr->A[i];
+        i--;
+    }
+    arr->A[i + 1] = x;
+    arr->length++;
+}
+
+int isSorted(struct Array arr)
+{
+    int i;
+    for (i = 0; i < arr.length - 1; i++)
+    {
+        if (arr.A[i] > arr.A[i + 1])
+            return 0;
+    }
+    return 1;
+}
+
+void Rearrange(struct Array* arr)
+{
+    int i, j;
+    i = 0;
+    j = arr->length - 1;
+
+    while (i < j)
+    {
+        while (arr->A[i] < 0)i++;
+        while (arr->A[j] >= 0)j--;
+        if (i < j)swap(&arr->A[i], &arr->A[j]);
+    }
+
+}
+
+struct Array* Merge(struct Array* arr1, struct Array* arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array* arr3 = (struct Array*)malloc(sizeof(struct Array));
+
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else
+            arr3->A[k++] = arr2->A[j++];
+    }
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+    for (; j < arr2->length; j++)
+        arr3->A[k++] = arr2->A[j];
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = 10;
+
+    return arr3;
+}
+
+struct Array* Union(struct Array* arr1, struct Array* arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array* arr3 = (struct Array*)malloc(sizeof(struct Array));
+
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else if (arr2->A[j] < arr1->A[i])
+            arr3->A[k++] = arr2->A[j++];
+        else
+        {
+            arr3->A[k++] = arr1->A[i++];
+            j++;
+        }
+    }
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+    for (; j < arr2->length; j++)
+        arr3->A[k++] = arr2->A[j];
+
+    arr3->length = k;
+    arr3->size = 10;
+
+    return arr3;
+}
+
+struct Array* Intersection(struct Array* arr1, struct Array* arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array* arr3 = (struct Array*)malloc(sizeof(struct Array));
+
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            i++;
+        else if (arr2->A[j] < arr1->A[i])
+            j++;
+        else if(arr1->A[i]==arr2->A[j])
+        {
+            arr3->A[k++] = arr1->A[i++];
+            j++;
+        }
+    }
+
+    arr3->length = k;
+    arr3->size = 10;
+
+    return arr3;
+}
+
+struct Array* Difference(struct Array* arr1, struct Array* arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array* arr3 = (struct Array*)malloc(sizeof(struct Array));
+
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else if (arr2->A[j] < arr1->A[i])
+            j++;
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+
+    arr3->length = k;
+    arr3->size = 10;
+
+    return arr3;
+}
+
 int main()
 {
-    struct Array arr = { {23,3,14,25,6},10,5 };
+    struct Array arr1 = { {2,6,10,15,25},10,5 };
+    struct Array arr2 = { {3,6,7,15,20},10,5 };
+    struct Array* arr3;
     //int n, i;
 
     /*
@@ -190,9 +362,9 @@ int main()
     //printf("%d\n", Delete(&arr, 0));
     
     //Set(&arr, 0, 15);
-    printf("%f\n", Avg(arr));
-
-    Display(arr);
+    //printf("%f\n", Avg(arr));
+    arr3=Difference(&arr1, &arr2);
+    Display(*arr3);
     
     return 0;
 }
